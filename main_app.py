a21c39ef26b446a9584f40d00d3e38a201383bc6
shobhit786 <shobhitsingh232@gmail.com>
2022-11-20 19:20:23 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -11,4 +12,5 @@ def webhook():
     return 'webhook App with Python Flask!'
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
